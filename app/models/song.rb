0c3828dbafbc6dd9ec1276d527dc6356c9a4ccaf
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-24 02:29:18 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,15 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
+<<<<<<< HEAD
     drake = Artist.find_or_create_by(name: 'Drake')
     drake.songs << self
+=======
+    if Artist.all.find {|artist| artist.name == 'Drake'}
+      drake.songs << self
+    else
+      drake = Artist.create(:name=>'Drake')
+      drake.songs << self
+>>>>>>> c5beedfd4ed8bf371adcc276da934f4671e84277
   end
 end
